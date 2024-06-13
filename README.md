b17a784a9986404a136aa80bf0edfb8b3fb5c202
daniel.souza <daniel.ssouza.f@gmail.com>
2024-06-13 13:17:25 -0300

    files

---
+++ README.md
@@ -0,0 +1,3 @@
+# A
+
+requer: alacritty
--- scripts/sleep.sh
+++ scripts/sleep.sh
@@ -1,3 +1,4 @@
+# I'm not using this shit
 swayidle -w timeout 600 'hyprctl dispatch dpms off' \
             timeout 900 'systemctl suspend' \
             before-sleep 'swaylock -f -c 901900' &
